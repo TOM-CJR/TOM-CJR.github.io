524d1749fc0e5927b7f60c02ee04e3ee37fd6a71
TomCJR <3093403156@qq.com>
2024-12-15 21:53:41 +0800

    圣诞更新
    第一次更新

---
+++ Love_files/myImge.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const img = document.getElementById("myImg");
+    //添加点击事件监听器
+    img.addEventListener("click", function () {
+        alert("你真棒！");
+    });
+});
