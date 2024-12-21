298a0a9d2d558a8012d11cf8465d45515f66978a
TomCJR <3093403156@qq.com>
2024-12-21 12:13:48 +0800

    重要更新
    往事展示 神奇效果和登录界面

--- Love_files/myImge.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    const img = document.getElementById("myImg");
-    //添加点击事件监听器
-    img.addEventListener("click", function () {
-        alert("你真棒！");
-    });
-});
